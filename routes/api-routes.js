5db2d93b394fe5c2ab092f597027f28fa426156c
julhanlon <jhanlon289@gmail.com>
2020-06-10 20:50:36 -0700

    added file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 router.get("/api/test", (req, res) => {
   console.log("api has been hit");
@@ -17,5 +21,26 @@ router.get("/api/test3", (req, res) => {
   console.log("api has been hit");
   res.json({ message: "this is a third example" });
 });
+// endpoint /api/count
+router.get("/api/count", async (req, res) => {
+let countData = await readFileAsync("data.json");
+countData = JSON.parse(countData);
+countData.count++;
+
+await writeFileAsync("data.json", JSON.stringify(countData, null, 2));
+res.send(countData);
+  // fs.readFile("data.json", "utf8", (err, data) => {
+  //   if (err) throw err;
+  //   const currentCount - JSON.parse(data);
+  //   currentCount.count ++;
+
+  //   fs.writeFile("data.json", JSON.stringify(currentCount, null, 2), (err => {
+  //     if (err) throw err;
+  //     res.send(currentCount);
+  //   }))
+
+  // })
+ 
+})
 
 module.exports = router;
